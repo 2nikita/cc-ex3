c954c4f7a253695fbdd7fb6339efa9ecb8e37121
2nikita <nv.vedeneev@gmail.com>
2021-06-03 12:10:33 +0200

    created functions

--- product-descp-service/product_descp.js
+++ product-descp-service/product_descp.js
@@ -8,5 +8,21 @@ module.exports = function (options) {
 
 
     //To DO: add the pattern functions and describe the logic inside the function
+    function productURL(productID, respond) {
+        for (var i = 0; i < mockData.length; i++) {
+            var product = mockData[i];
+            if (product.product_id == productID) {
+                respond(null, { result: product.product_url });
+            }
+        }
+    }
 
+    function productName(productID, respond) {
+        for (var i = 0; i < mockData.length; i++) {
+            var product = mockData[i];
+            if (product.product_id == productID) {
+                respond(null, { result: product.product_name });
+            }
+        }
+    }
 }
--- product-price-service/product_price.js
+++ product-price-service/product_price.js
@@ -2,8 +2,15 @@ module.exports = function (options) {
     //Import the mock data json file
     const mockData = require('./MOCK_DATA.json');
     //To DO: Add the patterns and their corresponding functions
-
+    this.add('role:product,cmd:getProductPrice', productPrice);
 
     //To DO: add the pattern functions and describe the logic inside the function
-
+    function productPrice(productID, respond) {
+        for (var i = 0; i < mockData.length; i++) {
+            var product = mockData[i];
+            if (product.product_id == productID) {
+                respond(null, { result: product.product_price });
+            }
+        }
+    }
 }
